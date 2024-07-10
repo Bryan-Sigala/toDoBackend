3b71cef4046b8cb9db3549a2f99cf3bcc6b8282d
Bryan-Sigala <bryan.sigala@encora.com>
2024-07-10 14:14:54 -0600

    Add CrossOrigin to the client in Controller

--- src/main/java/com/toDoApp/toDoBackend/toDo/ToDoController.java
+++ src/main/java/com/toDoApp/toDoBackend/toDo/ToDoController.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/todos")
 public class ToDoController {
